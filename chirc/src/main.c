77b309ad56c25b153926308a189e33df8a858cb7
cchyla <cchyla@cs.uchicago.edu>
2020-01-09 16:22:54 -0600

    Other changes

--- chirc/src/main.c
+++ chirc/src/main.c
@@ -43,7 +43,11 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
 #include "log.h"
 
 
