80369a9d90ddab01098f2480acd8f6aff2be2314
Artur Edward Genser <aegenser@poliwrath.cs.uchicago.edu>
2020-01-16 10:52:14 -0600

    handler.h added with all specified cmds

---
+++ chirc/src/handler.h
@@ -0,0 +1,22 @@
+#include "message.h"
+
+#ifndef CHIRC_HANDLER_H
+#define CHIRC_HANDLER_H
+
+int handle_NICK(server_ctx *ctx, chirc_message_t *msg, user_t *user);
+int handle_USER(server_ctx *ctx, chirc_message_t *msg, user_t *user);
+int handle_QUIT(server_ctx *ctx, chirc_message_t *msg, user_t *user);
+int handle_PRIVMSG(server_ctx *ctx, chirc_message_t *msg, user_t *user);
+int handle_NOTICE(server_ctx *ctx, chirc_message_t *msg, user_t *user);
+int handle_PING(server_ctx *ctx, chirc_message_t *msg, user_t *user);
+int handle_PONG(server_ctx *ctx, chirc_message_t *msg, user_t *user);
+int handle_LUSERS(server_ctx *ctx, chirc_message_t *msg, user_t *user);
+int handle_WHOIS(server_ctx *ctx, chirc_message_t *msg, user_t *user);
+int handle_JOIN(server_ctx *ctx, chirc_message_t *msg, user_t *user);
+int handle_PART(server_ctx *ctx, chirc_message_t *msg, user_t *user);
+int handle_PART(server_ctx *ctx, chirc_message_t *msg, user_t *user);
+int handle_MODE(server_ctx *ctx, chirc_message_t *msg, user_t *user);
+int handle_LIST(server_ctx *ctx, chirc_message_t *msg, user_t *user);
+int handle_OPER(server_ctx *ctx, chirc_message_t *msg, user_t *user);
+
+#endif /* CHIRC_MESSAGE_H */
